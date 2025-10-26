3216fe5c1a8a63f982ae976c681f1186a9e8ec83
jobet1995 <jobetcasquejo221@gmail.com>
2025-10-26 21:24:02 +0800

    initialize

---
+++ everstock/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-v&6noz0*kuurn^+-mq4_78r8upv((r#0!hic&hgqcwg63ap6)k"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
